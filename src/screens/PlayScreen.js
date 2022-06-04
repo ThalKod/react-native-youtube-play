b7f18571a28c9966c86e2a21d95d58d8e5a8187e
Thal <tmarcelin@thal.tech>
2022-06-04 15:06:22 +0200

    add PlayScreen.js

---
+++ src/screens/PlayScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const PlayScreen = () => {
+  return (
+    <View>
+      <Text>Hello World !</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default PlayScreen;
