da30e6692a5cfef067f1634dd974d56ad5140062
Thal <tmarcelin@thal.tech>
2022-06-22 14:18:17 +0200

    handling contril show

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -6,6 +6,7 @@ import {
   Dimensions,
   Slider,
   TouchableNativeFeedback,
+  TouchableWithoutFeedback
 } from 'react-native';
 import Video from 'react-native-video';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -21,6 +22,7 @@ const VideoPlayer = () => {
   const [loaded, setLoaded] = useState(false);
   const [overlay, setOverlay] = useState(true);
   const [fullscreen, setFullScreen] = useState(false);
+  const [showControl, setShowControl] = useState(false);
 
   let player = useRef(null);
 
@@ -29,30 +31,32 @@ const VideoPlayer = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <Video
-        fullscreen={fullscreen}
-        paused={paused}
-        ref={(ref) => player = ref}
-        source={videoSample}
-        style={styles.video}
-        resizeMode='cover'
-        onLoad={({ duration }) =>  { setDuration(duration); setLoaded(true) } }
-        onBuffer={(b) => console.log("b" + b)}
-        onError={(e) => console.log("Error " + JSON.stringify(e))}
-        controls={false}
-        onProgress={({currentTime}) => setCurrentTime(currentTime)}
-      />
-      {loaded &&
-      <Controls
-        duration={duration}
-        currentTime={currentTime}
-        onSlide={onSlide}
-        paused={paused}
-        setPaused={setPaused}
-      />
-      }
-    </View>
+    <TouchableWithoutFeedback onPress={() => setShowControl(!showControl)}>
+      <View style={styles.container}>
+        <Video
+          fullscreen={fullscreen}
+          paused={paused}
+          ref={(ref) => player = ref}
+          source={videoSample}
+          style={styles.video}
+          resizeMode='cover'
+          onLoad={({ duration }) =>  { setDuration(duration); setLoaded(true) } }
+          onBuffer={(b) => console.log("b" + b)}
+          onError={(e) => console.log("Error " + JSON.stringify(e))}
+          controls={false}
+          onProgress={({currentTime}) => setCurrentTime(currentTime)}
+        />
+        {loaded && showControl &&
+        <Controls
+          duration={duration}
+          currentTime={currentTime}
+          onSlide={onSlide}
+          paused={paused}
+          setPaused={setPaused}
+        />
+        }
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
