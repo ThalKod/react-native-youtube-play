8ec48277c5b879ccd1b640ddd50aad7d9abb119c
Thal <tmarcelin@thal.tech>
2022-07-04 15:51:03 +0200

    add VideoSingle screen

---
+++ src/components/VideoSingle.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet
+} from "react-native";
+
+
+const VideoSingle = () => {
+  return (
+    <View>
+      <Text>Hello World !</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
+
+export default VideoSingle;
